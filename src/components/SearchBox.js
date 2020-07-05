602d98bf29ab65cb8da16f16df1ce413c1020ce6
lukindo <lukindo.mbuli@gmail.com>
2020-07-05 09:09:33 +0200

    use aria-label instead and fix image size parameter

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,11 +4,8 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
             <form>
-                <label
-                style={{display:'none'}}
-                for="searchbox"
-                >Search: </label>
                 <input
+                    aria-label="searchbox"
                     id="searchbox"
                     onChange={searchChange}
                     className="pa3 ba b--green bg-lightest-blue"
