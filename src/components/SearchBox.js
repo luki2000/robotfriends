e6d59072d379891bb4699d34345b89afef41638f
lukindo <lukindo.mbuli@gmail.com>
2020-07-05 08:14:37 +0200

    added label to search input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,15 +3,17 @@ import React from 'react';
 const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
-            <label 
-            for="searchBox"
-            style={{display:'none'}} >Search</label>
-            <input
-                name="searchBox"
-                onChange={searchChange}
-                className="pa3 ba b--green bg-lightest-blue"
-                type="search" 
-                placeholder='search robots'/>
+            <form>
+                <label 
+                for="searchbox"
+                style={{display:'none'}} >Search</label>
+                <input
+                    name="searchbox"
+                    onChange={searchChange}
+                    className="pa3 ba b--green bg-lightest-blue"
+                    type="search" 
+                    placeholder='search robots'/>
+            </form>
         </div>
     )
 };
